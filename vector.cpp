b43c364f2ee83a714c3e90ff5228ace7ea6cd277
O R Imon <orimon9@gmail.com>
2024-06-19 11:19:38 +0300

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+
+{
+    int a[4];
+    vector<int>v;
+
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(5);
+    v.push_back(6);
+
+    for(int i=0;i<v.size();i++) cout<<v[i]<<" ";
+    cout<<endl;
+}

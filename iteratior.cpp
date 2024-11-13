188c0b532bb536bb3a5c52fccc0f40950fcffac8
O R Imon <orimon9@gmail.com>
2024-11-13 12:32:26 +0200

    Add files via upload

---
+++ iteratior.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string S = "Adnan Toky";
+	// iterating string in reverse
+	for(int i = S.size() - 1; i >= 0; i--) {
+		cout << S[i];
+	}
+	cout << endl;
+
+	return 0;
+}
